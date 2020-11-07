d68c4123dc7518c597a90bd5a15670913fd447ef
cyr-il <cyril.rufin@gmail.com>
2020-11-07 11:27:44 +0100

    entity, controller, psysh

---
+++ src/Controller/PinsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PinRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinsController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(PinRepository $pinRepository): Response
+
+    {
+        $pins = $pinRepository->findAll();
+        return $this->render('pins/index.html.twig', [
+            'pins' => $pins,
+        ]);
+    }
+}
